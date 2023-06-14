dcaa11773014594bc92f73e1d33f22ace1f2d0bb
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-06-14 12:04:41 +0300

    done

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,15 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     searchedMeals: [],
+    mealsByLetter: [],
   },
   getters: {},
   mutations: {
     setSearchMeals(state, meals) {
       state.searchedMeals = meals;
+    },
+    setMealsByLetter(state, meals) {
+      state.mealsByLetter = meals;
     },
   },
   actions: {
@@ -15,6 +19,11 @@ export default createStore({
       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
         .then((res) => res.json())
         .then((data) => context.commit("setSearchMeals", data.meals));
+    },
+    searchMealsByLetter(context, letter) {
+      fetch(`https:www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
+        .then((res) => res.json())
+        .then((data) => context.commit("setMealsByLetter", data.meals));
     },
   },
   modules: {},
--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>By Letter</div>
+  <div class="wrapper">
+    <h1>Meals By Letter</h1>
+    <div class="letters">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter in letters"
+        :key="letter"
+        >{{ letter }}</router-link
+      >
+    </div>
+    <div class="cardWrapper">
+      <MealItem v-for="meal in meals" :key="meal.idMeal" :meal="meal" />
+    </div>
+  </div>
 </template>
-<script setup></script>
-<style scoped></style>
+
+<script setup>
+import MealItem from "@/components/MealItem.vue";
+import store from "@/store";
+import { computed, watch } from "vue";
+import { useRoute } from "vue-router";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const meals = computed(() => store.state.mealsByLetter);
+const route = useRoute();
+watch(route, () => {
+  store.dispatch("searchMealsByLetter", route.params.letter);
+});
+</script>
+
+<style scoped>
+.wrapper {
+  margin-top: 32px;
+}
+h1 {
+  color: orange;
+}
+.letters {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-bottom: 16px;
+}
+a {
+  color: black;
+  text-decoration: none;
+}
+a:hover {
+  color: red;
+}
+.cardWrapper {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 16px;
+}
+</style>
