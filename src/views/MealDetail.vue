a0deead3258f5a89bd470894f5731a8ddf8dba4a
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-06-14 09:41:44 +0300

    mealDetail

--- src/views/MealDetail.vue
+++ src/views/MealDetail.vue
@@ -1,5 +1,75 @@
 <template>
-  <div>Meal Detail</div>
+  <div>
+    <div class="header">
+      <h1>{{ meal.strMeal }}</h1>
+      <img :src="meal.strMealThumb" />
+    </div>
+    <div class="middleWraper">
+      <div><strong>Category:</strong>{{ meal.strCategory }}</div>
+      <div><strong>Area:</strong>{{ meal.strArea }}</div>
+      <div><strong>Tags:</strong>{{ meal.strTags }}</div>
+    </div>
+    <div class="bottomWrapper">
+      <div>
+        <h2>Ingredients</h2>
+        <ul>
+          <li v-for="(el, index) in new Array(20)">
+            <span v-if="meal[`strIngredient${index + 1}`]">
+              {{ index + 1 }}.{{ meal[`strIngredient${index + 1}`] }}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2>Measures</h2>
+        <ul>
+          <li v-for="(el, index) in new Array(10)">
+            <span v-if="meal[`strMeasure${index + 1}`]">
+              {{ index + 1 }}.{{ meal[`strMeasure${index + 1}`] }}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
-<script setup></script>
-<style scoped></style>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
+const meal = ref("");
+const route = useRoute();
+
+onMounted(() => {
+  fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${route.params.id}`
+  )
+    .then((res) => res.json())
+    .then((data) => (meal.value = data.meals[0]));
+});
+</script>
+
+<style scoped>
+li {
+  list-style-type: none;
+}
+.header {
+  width: 100%;
+  text-align: center;
+}
+.header img {
+  height: 400px;
+}
+.middleWraper {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  margin-bottom: 16px;
+}
+.bottomWrapper {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+</style>
